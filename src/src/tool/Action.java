4613d19d181076cea3d3fabda3fc671b4dcc659e
kawamata <ymg2365063@stu.o-hara.ac.jp>
2024-07-18 13:53:47 +0900

    ファイルを追加した

--- src/src/tool/Action.java
+++ src/src/tool/Action.java
@@ -4,5 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public abstract class Action {
-	public abstract void execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
+
+	public abstract void execute(
+			HttpServletRequest request, HttpServletResponse response
+		) throws Exception;
+
 }
